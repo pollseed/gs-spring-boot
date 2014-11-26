9f2d9656fa6c70dd30e5ec4a34e42f941ed09697
pollseed <cb.so.devope+0001@gmail.com>
2014-11-26 18:44:23 +0900

    Update HelloController.java

--- initial/src/main/java/hello/HelloController.java
+++ initial/src/main/java/hello/HelloController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
     
+    // routesのようなもの
+    
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+    
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hogehogehello!!!";
     }
     
 }
